564929e241612a95afb66f858910927233b7b3ae
matur <michael@isi.epu>
2013-06-11 14:19:30 +0200

    test

--- NetArticlesJ2EE-ejb/src/java/services/AbstractService.java
+++ NetArticlesJ2EE-ejb/src/java/services/AbstractService.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import org.hibernate.HibernateException;
 
 /**
  *
